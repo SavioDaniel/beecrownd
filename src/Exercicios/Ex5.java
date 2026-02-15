7e5933bd9faaca2cbde6182d85ba49f66c7b4593
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-02-15 14:09:52 -0300

    Create Ex5.java

---
+++ src/Exercicios/Ex5.java
@@ -0,0 +1,10 @@
+package Exercicios;
+
+public class Ex5 {
+    static void main(String[] args) {
+        int num1 = 54;
+        int num2 = 30;
+
+        System.out.println("O valor da multiplicação será: " + (num1 * num2));
+    }
+}
