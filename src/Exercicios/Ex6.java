50e02920bc25fde7bc229c8ca438d93d035b35e7
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-02-15 14:14:01 -0300

    Create Ex6.java

---
+++ src/Exercicios/Ex6.java
@@ -0,0 +1,11 @@
+package Exercicios;
+
+public class Ex6 {
+    static void main(String[] args) {
+        double num1 = 5.3;
+        double num2 = 8.4;
+
+
+        System.out.println("A media do aluno será: " + ((num1 + num2) / 2));
+    }
+}
