60bf3962396dbeda99a0c1b4c4726077bf3fc38e
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-02-16 11:18:13 -0300

    Update Ex12.java

--- src/Exercicios/Ex12.java
+++ src/Exercicios/Ex12.java
@@ -1,4 +1,17 @@
 package Exercicios;
-
+import java.util.Scanner;
 public class Ex12 {
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double raio = scanner.nextDouble();
+        double pi = 3.14159;
+
+        double volume = (4.0 / 3.0) * pi * raio * raio * raio;
+
+        System.out.printf("VOLUME = %.3f%n", volume);
+
+        scanner.close();
+
+    }
 }
