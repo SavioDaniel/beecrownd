9d0cbdfca50d5a6f5271ff81249cb5ca5aeee8ed
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-02-16 11:06:56 -0300

    Update Ex11.java

--- src/Exercicios/Ex11.java
+++ src/Exercicios/Ex11.java
@@ -1,4 +1,17 @@
 package Exercicios;
-
+import java.util.Scanner;
 public class Ex11 {
+    static void main(String[] args) {
+        Scanner scaner = new Scanner(System.in);
+        int codigo1 = scaner.nextInt();
+        int numero1 = scaner.nextInt();
+        double valorUnitario1 = scaner.nextDouble();
+
+        int codigo2 = scaner.nextInt();
+        int numero2 = scaner.nextInt();
+        double valorUnitario2 = scaner.nextDouble();
+
+        double valor = numero1 * valorUnitario1 + numero2 * valorUnitario2;
+        System.out.printf("VALOR A PAGAR: R$ %.2f%n", valor);
+    }
 }
