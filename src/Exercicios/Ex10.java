0d091c2f7dc4dea11c1637a80a3fba3bf6953299
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-02-16 09:23:11 -0300

    Update Ex10.java

--- src/Exercicios/Ex10.java
+++ src/Exercicios/Ex10.java
@@ -1,4 +1,17 @@
 package Exercicios;
-
+import java.util.Scanner;
 public class Ex10 {
+    static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        String nome = scanner.next();
+        double salario = scanner.nextDouble();
+        double vendas = scanner.nextDouble();
+
+        double total = salario + (vendas * 0.15);
+
+        System.out.printf("TOTAL = R$ %.2f%n", total);
+
+        scanner.close();
+    }
 }
