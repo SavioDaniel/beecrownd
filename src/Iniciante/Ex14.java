ac2cb6f7d6392b2d20263cf5e2aa029b8d918b4e
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-02-18 15:16:09 -0300

    Create Ex14.java

---
+++ src/Iniciante/Ex14.java
@@ -0,0 +1,18 @@
+package Iniciante;
+import java.util.Scanner;
+public class Ex14 {
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        int maiorAB = (a + b + Math.abs(a - b)) / 2;
+        int maiorABC = (maiorAB + c + Math.abs(maiorAB - c)) / 2;
+
+        System.out.println(maiorABC + " eh o maior");
+
+        scanner.close();
+    }
+}
