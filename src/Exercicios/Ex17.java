7fa63be27a559e4826f995552268e1d4923aa7ee
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-02-18 15:14:00 -0300

    Create Ex17.java

---
+++ src/Exercicios/Ex17.java
@@ -0,0 +1,13 @@
+package Exercicios;
+import java.util.Scanner;
+public class Ex17 {
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int X = scanner.nextInt();
+        //int Y = scanner.nextInt();
+
+        int soma = X * 2;
+
+        System.out.printf("%d minutos %n", soma);
+    }
+}
