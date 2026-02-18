c871e962c46fbaea87593449defd164bcf1bbc24
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-02-18 15:16:00 -0300

    Create Ex10.java

---
+++ src/Iniciante/Ex10.java
@@ -0,0 +1,17 @@
+package Iniciante;
+import java.util.Scanner;
+public class Ex10 {
+    static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        String nome = scanner.next();
+        double salario = scanner.nextDouble();
+        double vendas = scanner.nextDouble();
+
+        double total = salario + (vendas * 0.15);
+
+        System.out.printf("TOTAL = R$ %.2f%n", total);
+
+        scanner.close();
+    }
+}
