a0f441841406a68cbca95a523df37ba98d1bb208
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-02-18 15:16:05 -0300

    Create Ex12.java

---
+++ src/Iniciante/Ex12.java
@@ -0,0 +1,17 @@
+package Iniciante;
+import java.util.Scanner;
+public class Ex12 {
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double raio = scanner.nextDouble();
+        double pi = 3.14159;
+
+        double volume = (4.0 / 3.0) * pi * raio * raio * raio;
+
+        System.out.printf("VOLUME = %.3f%n", volume);
+
+        scanner.close();
+
+    }
+}
