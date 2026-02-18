4881e698270c7bae16e3d8f367d89d27fd3e5487
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-02-18 15:13:56 -0300

    Create Ex16.java

---
+++ src/Exercicios/Ex16.java
@@ -0,0 +1,17 @@
+package Exercicios;
+import java.util.Scanner;
+public class Ex16 {
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double X1 = scanner.nextDouble();
+        double X2 = scanner.nextDouble();
+        double Y1 = scanner.nextDouble();
+        double Y2 = scanner.nextDouble();
+
+        double soma = ((X1 - X2) * (X1 - X2) ) + ( (Y1 - Y2) * (Y1 - Y2));
+        double raiz = Math.sqrt(soma);
+
+        System.out.printf("%.4f%n",raiz);
+
+    }
+}
