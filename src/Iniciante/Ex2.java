ba34d55f06a685fe7f5d947748bb422c5dcff772
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-02-18 15:16:23 -0300

    Create Ex2.java

---
+++ src/Iniciante/Ex2.java
@@ -0,0 +1,13 @@
+package Iniciante;
+import  java.util.Scanner;
+public class Ex2 {
+    static void main() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int X = A + B;
+        System.out.println("X = " + X);
+    }
+}
