42dec7d2748208d01ae34a31909a5d69d61d02d7
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-02-18 15:16:25 -0300

    Create Ex3.java

---
+++ src/Iniciante/Ex3.java
@@ -0,0 +1,18 @@
+package Iniciante;
+import java.util.Scanner;
+
+public class Ex3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double raio = scanner.nextDouble();
+        double pi = 3.14159;
+
+        double area = pi * raio * raio;
+
+        System.out.printf("A=%.4f%n", area);
+
+        scanner.close();
+    }
+}
+
