1c5d038446250c3a3d58e24df237661731ca0399
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-02-21 09:13:07 -0300

    Update Ex20.java

--- src/Iniciante/Ex20.java
+++ src/Iniciante/Ex20.java
@@ -5,17 +5,20 @@ public class Ex20 {
         Scanner scanner = new Scanner(System.in);
         int tempo = scanner.nextInt();
 
-        if (tempo >= 3600){
-            int horas = tempo / 3600;
-            System.out.println(horas);
-        } else if (tempo >= 60) {
-            int min = tempo / 60;
-            System.out.println(min);
-        } else if (tempo > 0 && tempo < 60) {
-            int seg = tempo / 1;
-            System.out.println(seg);
+        while (true){
+            if (tempo >= 3600){
+                int horas = tempo / 3600;
+                System.out.println(horas);
+            } else if (tempo >= 60) {
+                int min = tempo / 60;
+                System.out.println(min);
+            } else if (tempo > 0 && tempo < 60) {
+                int seg = tempo / 1;
+                System.out.println(seg);
+            }else {
+                break;
+            }
         }
-
 
     }
 }
