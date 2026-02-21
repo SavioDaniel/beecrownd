0a58e449f14c670476e086cf31846eceb76acc5b
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-02-21 08:45:19 -0300

    Create Ex20.java

---
+++ src/Iniciante/Ex20.java
@@ -0,0 +1,21 @@
+package Iniciante;
+import java.util.Scanner;
+public class Ex20 {
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int tempo = scanner.nextInt();
+
+        if (tempo >= 3600){
+            int horas = tempo / 3600;
+            System.out.println(horas);
+        } else if (tempo >= 60) {
+            int min = tempo / 60;
+            System.out.println(min);
+        } else if (tempo > 0 && tempo < 60) {
+            int seg = tempo / 1;
+            System.out.println(seg);
+        }
+
+
+    }
+}
