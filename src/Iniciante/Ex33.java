5d7857654b2ae77ba75d7bd1d019f19d667c6894
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-03-09 19:16:26 -0300

    Create Ex33.java

---
+++ src/Iniciante/Ex33.java
@@ -0,0 +1,22 @@
+package Iniciante;
+import java.util.Scanner;
+public class Ex33 {
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int inicio = scanner.nextInt();
+        int fim = scanner.nextInt();
+
+        int duracao;
+
+        if (inicio < fim) {
+            duracao = fim - inicio;
+        } else {
+            duracao = 24 - inicio + fim;
+        }
+
+        System.out.println("O JOGO DUROU " + duracao + " HORA(S)");
+
+        scanner.close();
+    }
+}
