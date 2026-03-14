992729b1fdba3630fcf1a55402769e54c8aec9ee
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-03-14 18:34:40 -0300

    Ex 36
    exemplo concluído com sucesso

--- src/Iniciante/Ex36.java
+++ src/Iniciante/Ex36.java
@@ -5,17 +5,54 @@ import java.util.Scanner;
 public class Ex36 {
     static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String nome1 = scanner.next();
-        String nome2 = scanner.next();
-        String nome3 = scanner.next();
 
-        if (nome1 == "vertebrado"){
-            if (nome2 == "ave" || nome2 == "mamifero"){
-                if (nome3 == "onivoro" || nome3 = "carnivoro"){
+        String a = scanner.nextLine();
+        String b = scanner.nextLine();
+        String c = scanner.nextLine();
 
+        if (a.equals("vertebrado")) {
+
+            if (b.equals("ave")) {
+
+                if (c.equals("carnivoro")) {
+                    System.out.println("aguia");
+                } else {
+                    System.out.println("pomba");
                 }
-                if (nome3 == "")
+
+            } else if (b.equals("mamifero")) {
+
+                if (c.equals("onivoro")) {
+                    System.out.println("homem");
+                } else {
+                    System.out.println("vaca");
+                }
+
             }
+
+        } else if (a.equals("invertebrado")) {
+
+            if (b.equals("inseto")) {
+
+                if (c.equals("hematofago")) {
+                    System.out.println("pulga");
+                } else {
+                    System.out.println("lagarta");
+                }
+
+            } else if (b.equals("anelideo")) {
+
+                if (c.equals("hematofago")) {
+                    System.out.println("sanguessuga");
+                } else {
+                    System.out.println("minhoca");
+                }
+
+            }
+
         }
+
+        scanner.close();
+    
     }
 }
