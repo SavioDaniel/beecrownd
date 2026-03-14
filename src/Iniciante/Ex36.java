4166b4bde86fe6682d1651b058cc9e26b75fcdb7
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-03-14 18:13:58 -0300

    Create Ex36.java

---
+++ src/Iniciante/Ex36.java
@@ -0,0 +1,21 @@
+package Iniciante;
+
+import java.util.Scanner;
+
+public class Ex36 {
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String nome1 = scanner.next();
+        String nome2 = scanner.next();
+        String nome3 = scanner.next();
+
+        if (nome1 == "vertebrado"){
+            if (nome2 == "ave" || nome2 == "mamifero"){
+                if (nome3 == "onivoro" || nome3 = "carnivoro"){
+
+                }
+                if (nome3 == "")
+            }
+        }
+    }
+}
